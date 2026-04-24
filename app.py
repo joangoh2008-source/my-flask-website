9897989e22bc7f0993b1e6ce3fe0e3343d8f6766
Joan <joangoh2008@gmail.com>
2026-04-24 21:49:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)

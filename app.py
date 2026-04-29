de6d4af7cde0d87717a61b6db73b73c2d2ce47fc
Joan <joangoh2008@gmail.com>
2026-04-29 11:35:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, GitHub World!"
+    return "Hello, welcome to the restaurant!"
 
 if __name__ == "__main__":
     app.run(debug=True)

b9f4df39321b1394e811a9e52fc5483f85931649
Joan <joangoh2008@gmail.com>
2026-04-29 11:01:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return "Hello, GitHub World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)

a767d9185e2ff13521a43935b235a831845fac01
Joan <joangoh2008@gmail.com>
2026-04-29 11:33:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask, render_template, request
-import os
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
-
-@app.route("/order", methods=["POST"])
-def order():
-    name = request.form.get("name")
-    quantity = request.form.get("quantity")
-    return f"Thank you {name}, your order of {quantity} bag(s) is received!"
+    return "Hello, GitHub World!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
